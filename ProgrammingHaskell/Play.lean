eb6a9783557c88139494c61a6fbe4ea601c92e77
Seasawher <tellagoodstory@icloud.com>
2025-02-25 07:17:04 +0900

    空白

---
+++ ProgrammingHaskell/Play.lean
@@ -0,0 +1,30 @@
+private def eratosthenesAux (n : Nat) : Array Bool := Id.run do
+  let mut isPrime := Array.mkArray (n + 1) true
+
+  isPrime := isPrime.set! 0 false
+  isPrime := isPrime.set! 1 false
+
+  for p in [2 : n + 1] do
+    if not isPrime[p]! then
+      continue
+
+    if p ^ 2 > n then
+      break
+
+    let mut q := p * p
+    while q <= n do
+      isPrime := isPrime.set! q false
+      q := q + p
+
+  return isPrime
+
+/-- エラトステネスの篩 -/
+def eratosthenes (n : Nat) : Array Nat :=
+  eratosthenesAux n
+    |>.zipIdx
+    |>.filterMap fun ⟨isPrime, i⟩ =>
+      if isPrime then some i else none
+
+#guard eratosthenes 10 = #[2, 3, 5, 7]
+
+#guard (eratosthenes 100).size = 25
